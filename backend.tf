f49c8f73f11ce70d54d1d03dbe2bf10faba8dcb1
Abhiramgc <36700010+Abhiramgc@users.noreply.github.com>
2022-04-27 08:50:36 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform {
+/*terraform {
   backend "s3" {
     bucket         = "terraform-backends3-2022"
     key            = "terraform-aws-eks-workshop.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
-}
+}*/
